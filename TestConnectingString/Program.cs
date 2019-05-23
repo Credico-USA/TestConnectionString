b69413f64fca41f23314e5d74b1fb05ae19c7ca3
mtariqahmed <tariq.ahmed@credicousa.com>
2019-05-23 10:11:34 -0500

    initial commit

---
+++ TestConnectingString/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * tariq.ahmed@crediocusa.com
+ * */
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Use this tool to test out a db connection string
+/// </summary>
+namespace TestConnectingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbTest"].ConnectionString))
+                {
+                    Console.WriteLine("Trying to connect....");
+                    conn.Open();
+                    Console.WriteLine("connection string works...");
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "Invalid Connection String");
+                Console.ReadLine();
+            }
+        }
+    }
+}
